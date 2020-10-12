2c1a1985a7fa7d7f8154b09b5d43758081449aae
Kai Sassnowski <me@kai-sassnowski.com>
2020-10-12 12:19:43 +0200

    Add _proper_ favicon handling

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,45 @@
 module.exports = {
   title: "Venture",
-  description: "A package to manage complex workflows built on top off Laravel's queue.",
+  description:
+    "A package to manage complex workflows built on top off Laravel's queue.",
+
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "link",
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#3a0839" },
+    ],
+    ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+    ["meta", { name: "msapplication-TileColor", content: "#3a0839" }],
+    ["meta", { name: "msapplication-config", content: "/browserconfig.xml" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
+  ],
 
   themeConfig: {
     repo: "ksassnowski/venture",
@@ -29,9 +68,7 @@ module.exports = {
       {
         title: "Configuration",
         collapsable: false,
-        children: [
-          ["/configuration/table-names", "Changing the table names"],
-        ],
+        children: [["/configuration/table-names", "Changing the table names"]],
       },
     ],
   },
